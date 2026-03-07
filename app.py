da33370ee71be4f084dfadbceea64fcc8fb2beea
JBK <jaebin0815@naver.com>
2026-03-07 23:26:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,11 +25,23 @@ st.set_page_config(
 )
 
 st.markdown("""
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-59VPBW5K');</script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59VPBW5K"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <style>
     div[data-testid="stToolbarActions"] { display: none !important; }
-    button[data-testid="manage-app-button"] { display: none !important; }
-    [data-testid="manage-app-button"] { display: none !important; }
-    footer { display: none !important; }
+    button[data-testid="manage-app-button"],
+    ._terminalButton_rix23_138 { display: none !important; }
     div[data-testid="stMainMenu"] { display: none !important; }
     </style>
 """, unsafe_allow_html=True)
