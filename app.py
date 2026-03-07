a193f39788b82167c2f77d3beafe3b014b1ab408
JBK <jaebin0815@naver.com>
2026-03-07 23:40:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,20 +24,20 @@ st.set_page_config(
     layout="wide",
 )
 
+components.html("""
+    <script>
+    (function(w,d,s,l,i){
+        var p = w.parent || w;
+        p[l]=p[l]||[];p[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+    })(window.parent,window.parent.document,'script','dataLayer','GTM-59VPBW5K');
+    </script>
+""", height=0)
+
 st.markdown("""
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-59VPBW5K');</script>
-    <!-- End Google Tag Manager -->
-
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59VPBW5K"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-
     <style>
     div[data-testid="stToolbarActions"] { display: none !important; }
     button[data-testid="manage-app-button"],
