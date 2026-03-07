1d02da96230d7435651290a08aaac0c21b6e38b8
JBK <jaebin0815@naver.com>
2026-03-07 13:26:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ st.markdown("""
     <style>
     div[data-testid="stToolbarActions"] { display: none !important; }
     button[data-testid="manage-app-button"] { display: none !important; }
+    [data-testid="manage-app-button"] { display: none !important; }
+    footer { display: none !important; }
     div[data-testid="stMainMenu"] { display: none !important; }
     </style>
 """, unsafe_allow_html=True)
